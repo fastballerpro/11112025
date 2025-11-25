1d1baeb256e1537550374f5f9b4b03549d5e95d3
John Appleseed <john@gmail.com>
2025-11-25 18:29:33 +0200

    third revision

--- revision/main.py
+++ revision/main.py
@@ -1,24 +1,6 @@
-
-
 from utils import is_strong_password, has_duplicates, temperature_validator
 
 if __name__ == "__main__":
-    password = "StrongPass123"
-    if is_strong_password(password):
-        print("password is strong")
-    else:
-        print("password is easy")
-
-    
-    items = [1, 2, 3, 2]
-    if has_duplicates(items):
-        print("there are duplicates")
-    else:
-        print("there arent any duplicates")
-
-  
-    temp = 25
-    if temperature_validator(temp):
-        print("temperature is warm")
-    else:
-        print("temperature is cold")
+    print(is_strong_password("StrongPass123"))
+    print(has_duplicates([1, 2, 3, 2]))
+    print(temperature_validator(25))
