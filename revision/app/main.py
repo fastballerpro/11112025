13fc33283ae8c5556e432aa624abbdbea515c268
John Appleseed <john@gmail.com>
2025-11-25 18:25:51 +0200

    second revision

---
+++ revision/app/main.py
@@ -0,0 +1,24 @@
+
+
+from utils import is_strong_password, has_duplicates, temperature_validator
+
+if __name__ == "__main__":
+    password = "StrongPass123"
+    if is_strong_password(password):
+        print("password is strong")
+    else:
+        print("password is easy")
+
+    
+    items = [1, 2, 3, 2]
+    if has_duplicates(items):
+        print("there are duplicates")
+    else:
+        print("there arent any duplicates")
+
+  
+    temp = 25
+    if temperature_validator(temp):
+        print("temperature is warm")
+    else:
+        print("temperature is cold")
